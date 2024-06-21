53770eb1edfa28c80e6f4a275d77ba09aa2a4daf
Nayde227 <naylimar27@gmail.com>
2024-06-20 21:50:11 -0400

    aboutMe

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -12,18 +12,21 @@ function SobreMi() {
             <div className='homeText'>
                 <h2>Naylimar Alvarez Cermeño</h2>
                 <section className='bio'>
-                    <img src={aboutMe}/>
+                    <img className='foto1' src={aboutMe}/>
                     <p className='texto1'>
                         Violinista venezolana, graduada en Interpretación Musical
                         (Conservatorio de Música Simón Bolívar/ UNEARTE) ;
                         En el año 2015, debuta como solista con el concierto para violín y Orquesta de
                         Khachaturian con la orquesta Sinfónica Regional de los Altos Mirandinos.
+                        <hr></hr>
                         En su destacada carrera musical, ha tocado con diversas orquestas, nacionales e
                         internacionales como la Orquesta Sinfónica de Caracas, Orquesta Filarmónica de Santiago,
                         Orquesta Sinfónica Regional de los Altos Mirandinos, Orquesta Música Para la Integración;
                         de la mano de directores como Félix Briceño, Andrés Gonzalez, Antonio Mayorca,
                         Raúl A. Delgado, Dietrich Paredes, Diego Matheuz, Pedro Pablo Prudencio, Paolo Bortolameoli,
-                        Luis Toro, Alejandra Urrutia, Gustavo Dudamel. Durante toda su carrera musical,
+                        Luis Toro, Alejandra Urrutia, Gustavo Dudamel. 
+                        <hr></hr>
+                        Durante toda su carrera musical,
                         ha desempeñado también una importante labor como docente de violín, cuenta con 7 años de
                         experiencia enseñando este instrumento a niños, jóvenes y adultos.
                     </p>
