02948896d892a0323572c8264183599536ca0bda
Nayde227 <naylimar27@gmail.com>
2024-07-01 22:57:41 -0400

    add/events, class, aboutMe

--- src/components/Clases.jsx
+++ src/components/Clases.jsx
@@ -2,9 +2,31 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Nav from './nav'
 import '../Nav.css'
+import '../Clases.css'
+
+import violin from '../assets/violin.jpg'
 function Clases() {
-    return(
-        <><Nav></Nav></>
+    return (
+        <>
+
+            <Nav></Nav>
+            <section className='homeText'>
+                <h2>Clases Personalizadas</h2>
+                <div className='clasesText'>
+                    <img className='fotoV' src={violin}></img>
+
+                    <p>Ofrecemos clases personalizadas de una hora académica en nuestra academia de música.
+                        En estas sesiones, exploraremos teoría musical y lectura de partituras, complementadas con la práctica del instrumento.
+                        Cada clase está diseñada para adaptarse a las necesidades y objetivos individuales del estudiante, garantizando un aprendizaje integral y efectivo.
+                        ¡Únete a nosotros y lleva tu habilidad musical al siguiente nivel!
+
+                    </p>
+
+                </div>
+
+            </section>
+
+        </>
     )
 }
 export default Clases
--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import eventos from '../assets/eventos.jpg'
 import Nav from './nav'
 import '../Nav.css'
+import '../Eventos.css'
 function Eventos() {
-    return(
-        <><Nav></Nav></>
+    return (
+        <><Nav></Nav>
+            <section className='homeText'>
+                <h2>Eventos</h2>
+                <div className='eText'>
+                    <img className='imgE' src={eventos}></img>
+                    <p> 
+                        Si buscas añadir un toque especial y elegante a tu boda, aniversario, fiesta o cualquier
+                         otra ocasión especial, contamos con servicios de música en vivo para eventos privados.
+                         Nos adaptamos al tipo de celebración, a la música que quieras, y al estilo de ceremonia. 
+                            No te preocupes más, nosotros lo solventamos.
+                         
+                         Con un repertorio que podrás escoger, me aseguro de crear el ambiente perfecto para tu celebración.
+                         <br></br>
+                         ¡Estaré encantada de ser parte de tu evento!</p>
+                         
+                </div>
+                <a className='cotizaE' target="_blank" href='https://forms.gle/GiUKyzn1D1YYnsn18'> Cotiza Aquí Tu Evento</a>
+            </section>
+        </>
     )
 }
 export default Eventos
